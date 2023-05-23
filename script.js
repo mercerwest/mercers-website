fa1470d00e667018e7daa541fdad5c945e76a5fa
mercerwest <mercerwest2@gmail.com>
2023-05-23 19:36:27 -0400

    Update script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    var date = new Date();
-    var formattedDate = date.toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-    document.getElementById('date').innerHTML = "Last updated on " + formattedDate;
-};
